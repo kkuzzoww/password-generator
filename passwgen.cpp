8b92872cf1e71575722c23f37d06167ad98622a4
kkuzzoww <142544046+kkuzzoww@users.noreply.github.com>
2024-04-27 22:39:47 -0400

    Add files via upload

--- passwgen.cpp
+++ passwgen.cpp
@@ -1,3 +1,4 @@
+#include <cstdlib>
 #include <iostream>
 #include <algorithm>
 #include <random>
@@ -6,7 +7,10 @@ class PasswordGenerator {
 public:
     PasswordGenerator();
     PasswordGenerator(int length, bool useDigits, bool useLowercase, bool useSpecialChars, bool useUppercase)
-    : length(length), useDigits(useDigits), useLowercase(useLowercase), useSpecialChars(useSpecialChars), useUppercase(useUppercase) {}
+    : length(length), useDigits(useDigits), useLowercase(useLowercase), useSpecialChars(useSpecialChars), useUppercase(useUppercase) {
+        std::srand(std::time(NULL));
+    }
+
     std::string generatePassword();
 
 private:
@@ -23,23 +27,32 @@ private:
 std::string PasswordGenerator::generatePassword() {
     std::string password;
     for(int i = 0; i < length; ++i) {
+        if(i > 0 && i % 4 == 0) {
+            password += "-";
+        }
         password += genRandomChar();
     }
     return password;
 }
 
 char PasswordGenerator::genRandomChar() {
-    char ch;
-    for(int i = 0; i < length; ++i) {
-        
-    }
+    std::string chars;
+    if(useDigits) chars += "0123456789";
+    if(useSpecialChars) chars += "!@#$%^&*()-_=+[]{}|;:,.<>?";
+    if(useLowercase) chars += "abcdefghijklmnopqrstuvwxyz";
+    if(useUppercase) chars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    int randomIndex = rand() % chars.size();
+    return chars[randomIndex];
 }
 
 int main() 
 {
 
+    PasswordGenerator pw(16, true, true, false, true);
+    std::string password = pw.generatePassword();
+    std::cout << password << '\n';
 
-
-return 0;
+    return 0;
 
 }
